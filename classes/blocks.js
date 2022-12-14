9643f50ef1e5410b0a70ce24cd902586156cbf8f
Viktor Perov <minegoodfeeling@gmail.com>
2022-12-14 19:55:19 +0300

    finally, i did it!

--- classes/blocks.js
+++ classes/blocks.js
@@ -12,10 +12,6 @@ class Block {
 }
 
 export class VideoBlock extends Block {
-  constructor (value, options) {
-    super(value, options)
-  }
-
   toHTML () {
     return `<iframe width="100%" height="400" src="${this.value}" frameBorder="0"
                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
@@ -25,10 +21,6 @@ export class VideoBlock extends Block {
 }
 
 export class TitleBlock extends Block {
-  constructor (value, options) {
-    super(value, options)
-  }
-
   toHTML () {
     const { tag = 'h1', styles } = this.options
     return row(col(`<${tag}>${this.value}</{$tag}>`), css(styles))
@@ -36,10 +28,6 @@ export class TitleBlock extends Block {
 }
 
 export class ImageBlock extends Block {
-  constructor (value, options) {
-    super(value, options)
-  }
-
   toHTML () {
     const { imageStyles: is, alt = '', styles } = this.options
     return row(`<img src="${this.value}" alt="${alt}" style="${css(is)}"/>`, css(styles))
@@ -47,10 +35,6 @@ export class ImageBlock extends Block {
 }
 
 export class ImageActiveBlock extends ImageBlock {
-  constructor (value, options) {
-    super(value, options)
-  }
-
   toHTML () {
     const { imageStyles: is, alt = '', styles } = this.options
     return row(`<a href=""><img src="${this.value}" alt="${alt}" style="${css(is)}" /></a>`, css(styles))
@@ -58,10 +42,6 @@ export class ImageActiveBlock extends ImageBlock {
 }
 
 export class ColumnsBlock extends Block {
-  constructor (value, options) {
-    super(value, options)
-  }
-
   toHTML () {
     const html = this.value.map(item => `<div className="col-sm">${item}</div>`)
     return row(`${html.join('')}`, css(this.options.styles))
@@ -69,10 +49,6 @@ export class ColumnsBlock extends Block {
 }
 
 export class TextBlock extends Block {
-  constructor (value, options) {
-    super(value, options)
-  }
-
   toHTML () {
     return row(col(`<p>${this.value}</p>`), css(this.options.styles))
   }
